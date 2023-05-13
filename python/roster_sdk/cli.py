490983c71253dd025be9d3cee9f539ba5a5251a1
Evan Doyle <evanmdoyle@gmail.com>
2023-05-13 15:45:32 +0200

    Add CLI tool to easily create Entrypoint; TODO: langchain option, explicit config

---
+++ python/roster_sdk/cli.py
@@ -0,0 +1,37 @@
+import argparse
+import importlib
+
+from .entrypoint import Entrypoint
+from .interface import RosterAgentInterface
+
+
+# Is there a more standard way to do this?
+def import_object(path):
+    module_path, object_name = path.split(":")
+    module = importlib.import_module(module_path)
+    return getattr(module, object_name)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="CLI tool for Roster Agent")
+    parser.add_argument(
+        "agent_path", type=str, help="Python path to the RosterAgentInterface object"
+    )
+
+    args = parser.parse_args()
+
+    # Dynamically import the module and get the agent
+    agent = import_object(args.agent_path)
+
+    # Check if the agent conforms to the RosterAgentInterface
+    assert isinstance(
+        agent, RosterAgentInterface
+    ), "The provided agent does not conform to the RosterAgentInterface"
+
+    # Create and run the entrypoint
+    entrypoint = Entrypoint.from_env(agent=agent)
+    entrypoint.run()
+
+
+if __name__ == "__main__":
+    main()
